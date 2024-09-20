eb77bee974273eadf46b61343af5572751142aa0
dika841 <randikaa1999@gmail.com>
2024-09-20 18:05:26 +0700

    improve:next config

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -8,7 +8,7 @@ const Dashboard: NextPage = (): ReactElement => {
   return (
     <div className="text-slate-700 flex-1 flex flex-col justify-center h-full items-center">
       <h1 className="text-xl font-semibold">You need a partner to start chat</h1>
-      <h2  className="text-lg text-red-400">Add a friend first</h2>   
+      <h2  className="text-lg text-red-400">Select a friend or add one</h2>   
     </div>
   );
 };
